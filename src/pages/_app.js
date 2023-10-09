eb272d84c1ca145f773e41543ccb8fe032329551
Ondřej Zaplatílek <ondrej.zaplatilek@gmail.com>
2023-10-09 12:04:27 +0200

    fix styling

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -27,7 +27,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
   return(<>
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WTWWNMW7"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
    <GoogleAnalytics trackPageViews gaMeasurementId='G-TRFWGCFMHL' /> <Component {...pageProps} /></> );
 }
 
